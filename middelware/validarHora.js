755aa3f7a0e473966cec20a7a3ddcdd474a61b84
Tomas <tomascarrerasalbesa@gmail.com>
2026-01-04 11:08:13 +0100

    funciana todo comprobado

--- middelware/validarHora.js
+++ middelware/validarHora.js
@@ -3,11 +3,8 @@
 const getHora = require('./horaMiddelware');
 
 const validarHora = (req, res, next) => {
-    const hour = new Date().getHours();
-        if (hour < 12) {
-            return res.status(403).send(`<h5>Aún no son las 12 de la mañana.</h5>`);
-        }
-        
+    const horaValida = new Date().getHours() >= 12;
+
         next();
     }
 
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -4,10 +4,17 @@ const router = express.Router();
 const getHora = require('../middelware/horaMiddelware.js');
 
 router.get('/', (req, res) => {
-    res.send(`
+    res.status(req.horaValida ? 200 : 403).send(`
         <h1>Bienvenido</h1>
         <h4>${req.dateType}</h4>
-        <a href="/endRoute">Entrar</a>
+        ${
+            req.horaValida
+            ? `
+                <h4>Ya es la hora puedes Acceder:</h4>
+                <a href="/endRoute">Entrar</a>`
+            : `
+                <p>Aún no son las 12 de la mañana, todavía no puedes entrar.</p>`
+        }        
         `)
 })
 
